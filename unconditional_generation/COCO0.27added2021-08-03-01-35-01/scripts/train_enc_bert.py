2420ff98904f4488d1721489121415f0bfd0c51f
Danshi-Li <stanleyofussr@163.com>
2021-08-03 01:38:40 -0400

    checking bert enc design

--- unconditional_generation/COCO0.27added2021-08-03-01-35-01/scripts/train_enc_bert.py
+++ unconditional_generation/COCO0.27added2021-08-03-01-35-01/scripts/train_enc_bert.py
@@ -263,9 +263,8 @@ def save_model():
 def cal_norm(model):
     total_norm = 0
     for p in model.parameters():
-        if p.grad is not None:
-            param_norm = p.grad.data.norm(2)
-            total_norm += param_norm.item() ** 2
+        param_norm = p.grad.data.norm(2)
+        total_norm += param_norm.item() ** 2
     total_norm = total_norm ** (1. / 2)
     return total_norm
 
